0b78271d5da18c90bc9507e778395d3afbbbfab3
lichuan <lichuan@365rili.com>
2017-05-06 22:49:30 +0800

    换成h2数据库

--- product/src/main/java/com.study.springcloud.product/Application.java
+++ product/src/main/java/com.study.springcloud.product/Application.java
@@ -1,7 +1,11 @@
 package com.study.springcloud.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +15,27 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class Application {
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	@Bean
+    public CommandLineRunner init() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+
+                jdbcTemplate.update("CREATE TABLE `wine` (`id` int NOT NULL AUTO_INCREMENT,"
+                    + "`name` varchar(255),"
+                    + "`price` int,"
+                    + "PRIMARY KEY (`id`)\n"
+                    + ")");
+                jdbcTemplate.update("insert into wine(`name`, price) values('wine1', 10)");
+                jdbcTemplate.update("insert into wine(`name`, price) values('wine2', 30)");
+            }
+        };
+    }
 
 	public static void main(String[] args){
 		new SpringApplicationBuilder(Application.class).run(args);
