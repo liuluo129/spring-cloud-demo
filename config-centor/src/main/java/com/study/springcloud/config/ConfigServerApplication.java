4b95311a621ef9c709d61ff8023d034690c52a2c
chuan <lichuan@365rili.com>
2017-05-08 15:01:16 +0800

    扩展

--- config-centor/src/main/java/com/study/springcloud/config/ConfigServerApplication.java
+++ config-centor/src/main/java/com/study/springcloud/config/ConfigServerApplication.java
@@ -3,13 +3,30 @@ package com.study.springcloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ * /**
+ * 通过@EnableConfigServer注解激活配置服务.
+ * 说明：
+ * 在application.yml中有个git.uri的配置
+ * 获取git上的资源信息遵循如下规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ *
+ * 其中application对应spring cloud config中的name表示应用名
+ * profile表示对应的环境
+ * label表示分支名,默认是master
+ *
  * @author mushui
  * @created 2017-03-27 下午4:14
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class);
---
+++ product/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into wine(`name`, price) values('wine1', 10);
+insert into wine(`name`, price) values('wine2', 30);
