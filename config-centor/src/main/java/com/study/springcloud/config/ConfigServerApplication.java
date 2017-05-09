e97bd864b05d4cf94b36eed4ca1eeb09198bf072
chuan <lichuan@365rili.com>
2017-05-09 17:18:51 +0800

    format

--- config-centor/src/main/java/com/study/springcloud/config/ConfigServerApplication.java
+++ config-centor/src/main/java/com/study/springcloud/config/ConfigServerApplication.java
@@ -5,6 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
+ * /**
+ * 通过@EnableConfigServer注解激活配置服务.
+ * 说明：
+ * 在application.yml中有个git.uri的配置
+ * 获取git上的资源信息遵循如下规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ *
+ * 其中application对应spring cloud config中的name表示应用名
+ * profile表示对应的环境
+ * label表示分支名,默认是master
+ *
  * @author mushui
  * @created 2017-03-27 下午4:14
  */
--- server/src/main/java/com/study/springcloud/search/SearchController.java
+++ server/src/main/java/com/study/springcloud/search/SearchController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.study.springcloud.product.Product;
 import com.study.springcloud.product.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,11 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/search")
 public class SearchController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SearchController.class);
+
 	@Autowired
 	private ProductService productService;
 
 	@RequestMapping(value = "/go.do", method = RequestMethod.GET, params = {"key"})
 	public List<Product> go(String key) {
+		LOGGER.info("search request: {}", key);
 		return productService.listProduct(search(key));
 	}
 
