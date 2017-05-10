d1d5be8e9083b8c9c4341993d37db9bfce39ed3b
lichuan <lichuan@365rili.com>
2017-05-10 22:22:03 +0800

    groovy

--- gateway/src/main/java/com/study/springcloud/zuul/ZuulApiGatewayApplication.java
+++ gateway/src/main/java/com/study/springcloud/zuul/ZuulApiGatewayApplication.java
@@ -1,5 +1,9 @@
 package com.study.springcloud.zuul;
 
+import com.netflix.zuul.FilterFileManager;
+import com.netflix.zuul.FilterLoader;
+import com.netflix.zuul.groovy.GroovyCompiler;
+import com.netflix.zuul.groovy.GroovyFileFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -13,6 +17,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ZuulApiGatewayApplication {
   public static void main(String[] args) {
-	  SpringApplication.run(ZuulApiGatewayApplication.class, args);
+      FilterLoader.getInstance().setCompiler(new GroovyCompiler());
+      try {
+          FilterFileManager.setFilenameFilter(new GroovyFileFilter());
+          FilterFileManager.init(10,"/Users/mushui/zuul-filter");
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+      SpringApplication.run(ZuulApiGatewayApplication.class, args);
   }
 }
