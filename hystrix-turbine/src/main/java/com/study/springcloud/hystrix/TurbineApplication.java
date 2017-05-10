50f90d8909c057bcec23ce1ee78f11f982495df3
chuan <lichuan@365rili.com>
2017-05-10 17:27:03 +0800

    hystrix dashboard turbine

---
+++ hystrix-turbine/src/main/java/com/study/springcloud/hystrix/TurbineApplication.java
@@ -0,0 +1,16 @@
+package com.study.springcloud.hystrix;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 通过@EnableTurbine接口，激活对Turbine的支持。
+ */
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+	public static void main(String[] args) {
+	  new SpringApplicationBuilder(TurbineApplication.class).web(true).run(args);
+  }
+}
